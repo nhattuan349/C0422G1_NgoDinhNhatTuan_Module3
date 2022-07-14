66bfe99ffc84b37e69c1e937c4c3e8e8c4129094
unknown <nhattuanqlmt2@gmail.com>
2022-07-14 20:56:50 +0700

    update ss2

--- ss2/baitap/built_database.sql
+++ ss2/baitap/built_database.sql
@@ -11,6 +11,7 @@ c_age varchar(45)
 create table order_ss2(
 o_id int auto_increment primary key,
 c_id int,
+foreign key(c_id) references customer(c_id),
 o_date date,
 o_total_price int 
 );
@@ -26,6 +27,5 @@ o_id int ,
 p_id int ,
 od_qty varchar(45),
 primary key(o_id,p_id),
-foreign key(o_id) references o_id(o_id),
-foreign key(p_id) references p_id(p_id)
-);
+foreign key(o_id) references order_ss2(o_id),
+foreign key(p_id) references product(p_id));
